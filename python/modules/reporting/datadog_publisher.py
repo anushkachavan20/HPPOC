bc54e59bb2c44fe41c402557adad8bd71586a950
Anushka Chavan <chavananushka782@gmail.com>
2026-09-17 23:14:17 +0530

    Datadog dashboard changes

--- python/modules/reporting/datadog_publisher.py
+++ python/modules/reporting/datadog_publisher.py
@@ -720,17 +720,17 @@ class DatadogPublisher:
         for index, widget in enumerate(widgets):
             if index < 9:
                 widget["layout"] = {
-                    "x": (index % 3) * 8,
-                    "y": (index // 3) * 4,
-                    "width": 8,
-                    "height": 4,
+                    "x": (index % 3) * 16,
+                    "y": (index // 3) * 8,
+                    "width": 16,
+                    "height": 8,
                 }
             else:
                 widget["layout"] = {
-                    "x": ((index - 9) % 2) * 12,
-                    "y": 12 + ((index - 9) // 2) * 6,
-                    "width": 12,
-                    "height": 6,
+                    "x": ((index - 9) % 2) * 24,
+                    "y": 24 + ((index - 9) // 2) * 12,
+                    "width": 24,
+                    "height": 12,
                 }
 
         dashboard = {
